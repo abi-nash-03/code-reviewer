f1205ddaa84acf5c77c8308ef1a314b0f83667c3
abi-nash-03 <abinash@qoruz.com>
2025-08-30 22:29:32 +0530

    rename file

--- test/review.py
+++ test/review.py
@@ -15,9 +15,6 @@ load_dotenv()
 # create an app instance
 app = FastAPI()
 
-def get_staged_files():
-	result = subprocess.run(["git", "diff", "--cached", "--name-only"], capture_output=True, text=True)
-	return result.stdout.splitlines()
 
 def format_pr_diff(diff_text):
 	diff_text_json = json.loads(diff_text)
