4d93eb8bb63474472a3bc9eff7ef9830c48258e7
abi-nash-03 <abinash@qoruz.com>
2025-08-31 12:18:11 +0530

    add new file

---
+++ test.py
@@ -0,0 +1,29 @@
+  GNU nano 6.2                                                                       main.py                                                                                 
+import time
+from config.mysql import get_mysql_connection
+from models.app_version import AppVersion
+import os
+
+av = AppVersion()
+
+def main():
+    try:
+        conn1 = get_mysql_connection()
+        start_time = time.time()
+        versions = av.list_versions()
+        end_time = time.time()
+
+        print(f"Fetched {len(versions)} app versions in {end_time - start_time:.2f} seconds.")
+
+        print("conn1.open", conn1.open)
+        conn1.close()
+        print("conn1.open after close", conn1.open)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
